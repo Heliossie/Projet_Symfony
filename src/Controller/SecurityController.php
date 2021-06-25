527c41c1c69cd0741f3349d7290fc23485e63607
Guillaumo <74065707+Guillaumo@users.noreply.github.com>
2021-06-25 11:07:06 +0200

    fix 250621

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,36 +16,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/registration", name="app_registration")
-     */
-    public function registration(Request $request, EntityManagerInterface $entityManagerInterface)
-    {
-        $user = new Client();
-        $admin = new Admin();
-        $form1 = $this->createForm(CreateUserFormType::class, $user);
-        $form2 = $this->createForm(CreateAdminFormType::class, $admin);
-
-        $form1->handleRequest($request);
-        $form2->handleRequest($request);
-
-        dump($request);
-        if ($form1->isSubmitted() && $form1->isValid()) {
-            $entityManagerInterface->persist($user);
-            $entityManagerInterface->persist($admin);
-
-            $entityManagerInterface->flush();
-
-            return $this->redirectToRoute('parking');
-        }
-        dump($request);
-
-        return $this->render('security/registration.html.twig', [
-            'form1'=>$form1->createView(),
-            'form2' => $form2->createView()
-        ]);
-    }
-
-    /**
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
